feb1e3ea82db4762225f05493109514fb2e7ed57
PATEL VANSH <vanshpatel3175@gmail.com>
2026-06-16 09:28:02 +0530

    role add in api

--- school_admin/api_url.py
+++ school_admin/api_url.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .api import SchoolLoginAPIView
 
 urlpatterns = [
-    path('login/', SchoolLoginAPIView.as_view(), name='api_school_login'),
+    path('school_login/', SchoolLoginAPIView.as_view(), name='api_school_login'),
 ]
--- school_admin/serializer.py
+++ school_admin/serializer.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class SchoolLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+   
