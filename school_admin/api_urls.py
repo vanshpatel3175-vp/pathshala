34e65640780eaeb7131bdf5dc21c8ff8d4f3ab28
VeerPtl <07veerpatel@gmail.com>
2026-06-23 12:48:35 +0530

    fix varidy api1

--- school_admin/api_urls.py
+++ school_admin/api_urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('logout/', api.SchoolLogoutAPIView.as_view(), name='api_school_logout'),
     # New clean login & verify endpoints
     path('auth/login/', api.LoginAPIView.as_view(), name='api_login'),
+    path('auth/login', api.LoginAPIView.as_view()),
     path('auth/verify/', api.VerifyProfileAPIView.as_view(), name='api_verify_profile'),
+    path('auth/verify', api.VerifyProfileAPIView.as_view()),
 ]
